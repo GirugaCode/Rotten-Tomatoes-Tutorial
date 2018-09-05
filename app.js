4a3b939eaedfe033dfd085deef8fdb3d4ada82e1
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-09-04 17:06:49 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var exphbs = require('express-handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
 const express = require('express')
 const app = express()
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
+
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
