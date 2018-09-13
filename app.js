2ff084d6d318d8bb3ffedf196407d7a28c095e92
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-09-13 10:27:57 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -13,8 +13,27 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -28,6 +47,11 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
 })
 
 app.listen(3000, () => {
