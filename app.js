a2e8f49212b38df369dabaf35b51ce7edb058010
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-09-13 10:40:15 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
