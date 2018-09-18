5300e7291017043dd5949672d0a2a9854611b42d
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-09-18 01:08:09 -0700

    Added Test

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -89,5 +89,32 @@ it('should display new form on /reviews/new GET', (done) => {
           done();
         });
   });
+  // TEST UPDATE
+  it('should update a SINGLE review on /reviews/<id> PUT', (done) => {
+    var review = new Review(sampleReview);
+    review.save((err, data)  => {
+     chai.request(server)
+      .put(`/reviews/${data._id}?_method=PUT`)
+      .send({'title': 'Updating the title'})
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html
+        done();
+      });
+    });
+  });
   // TEST DELETE
+  it('should delete a SINGLE review on /reviews/<id> DELETE', (done) => {
+    var review = new Review(sampleReview);
+    review.save((err, data)  => {
+     chai.request(server)
+      .delete(`/reviews/${data._id}?_method=DELETE`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html
+        done();
+      });
+    });
+  });
+});
 });
